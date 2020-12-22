d301eb43b4539ae4bc4c4a1f1d6f7d7442f7f301
nikkipeel <npeel3497@gmail.com>
2020-12-22 08:37:45 -0500

    first commit

---
+++ javascript-and-jquery-book-code-0915/javascript-and-jquery-book-code-0915/c01/js/add-content.js
@@ -0,0 +1,19 @@
+/* This script is a greeting to the user upon 
+    the current time. */
+
+var today = new Date(); // Create a new Date object
+var hourNow = today.getHours(); // Find the current hour
+var greeting;
+
+// Display the appropriate greeting based on the current time
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
